0cfceb5a79fe89644e4d9a39fb727e328dc352fb
Александр Сенченко <senchenko.sashuta@list.ru>
2026-02-18 18:09:34 +0800

    Объявление переменных

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите прибытие поезда в часах");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите прибытие поезда в минутах");
+            var b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите отправление поезда в часах");
+            var c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите отправление поезда в часах");
+            var d = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите прибытие пассажира в часах");
+            var m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите прибытие пассажира в часах");
+            var n = int.Parse(Console.ReadLine());
+
         }
     }
 }
