04623b7aeada5a3c21522a40ce120d3cd289769e
Александр Сенченко <senchenko.sashuta@list.ru>
2026-02-18 22:53:47 +0800

    Переработана логика и выполнина валидация

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,24 +6,43 @@
         {
             Console.WriteLine("Введите прибытие поезда в часах: ");
             var a = int.Parse(Console.ReadLine());
+            if (a > 23 || a < 0)
+            {
+                Console.WriteLine("Вы не вошли в диапазон!");
+            }
             Console.WriteLine("Введите прибытие поезда в минутах: ");
             var b = int.Parse(Console.ReadLine());
+            if (b > 59 || b < 0)
+            {
+                Console.WriteLine("Вы не вошли в диапазон!");
+            }
             Console.WriteLine("Введите отправление поезда в часах: ");
             var c = int.Parse(Console.ReadLine());
+            if (c > 23 || c < 0)
+            {
+                Console.WriteLine("Вы не вошли в диапазон!");
+            }
             Console.WriteLine("Введите отправление поезда в минутах: ");
             var d = int.Parse(Console.ReadLine());
+            if (d > 59 || d < 0)
+            {
+                Console.WriteLine("Вы не вошли в диапазон!");
+            }
             Console.WriteLine("Введите прибытие пассажира в часах: ");
             var m = int.Parse(Console.ReadLine());
+            if (m > 23 || m < 0)
+            {
+                Console.WriteLine("Вы не вошли в диапазон!");
+            }
             Console.WriteLine("Введите прибытие пассажира в минутах: ");
             var n = int.Parse(Console.ReadLine());
-            if (a < m && b < n && c > m && d > n)
+            if (n > 59 || n < 0)
             {
-                Console.WriteLine("Поезд стоит на перроне");
+                Console.WriteLine("Вы не вошли в диапазон!");
             }
-            else
-            {
-                Console.WriteLine("Поезд уехал");
-            }
+            int countMinutesArrive = a * 60 + b;
+            int countMinutesDeparture = c * 60 + d;
+            int countMinutesPassenger = m * 60 + n;
         }
     }
 }
