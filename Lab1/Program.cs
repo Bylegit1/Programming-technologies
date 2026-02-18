f33cc362f24ebfb5cbd8ce5cc85dcb042bba21cc
Александр Сенченко <senchenko.sashuta@list.ru>
2026-02-18 18:29:15 +0800

    Задача почти решена, осталось реализовать валидацию

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,19 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите прибытие поезда в часах");
+            Console.WriteLine("Введите прибытие поезда в часах: ");
             var a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите прибытие поезда в минутах");
+            Console.WriteLine("Введите прибытие поезда в минутах: ");
             var b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите отправление поезда в часах");
+            Console.WriteLine("Введите отправление поезда в часах: ");
             var c = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите отправление поезда в часах");
+            Console.WriteLine("Введите отправление поезда в минутах: ");
             var d = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите прибытие пассажира в часах");
+            Console.WriteLine("Введите прибытие пассажира в часах: ");
             var m = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите прибытие пассажира в часах");
+            Console.WriteLine("Введите прибытие пассажира в минутах: ");
             var n = int.Parse(Console.ReadLine());
-
+            if (a < m && b < n && c > m && d > n)
+            {
+                Console.WriteLine("Поезд стоит на перроне");
+            }
+            else
+            {
+                Console.WriteLine("Поезд уехал");
+            }
         }
     }
 }
